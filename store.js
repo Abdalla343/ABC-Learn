81c388dada7887b26ecc5d9b281225895015d4c8
Abdalla343 <Abdallaahmed2268199@gmail.com>
2024-07-13 19:43:38 +0300

    sss

--- store.js
+++ store.js
@@ -4,28 +4,70 @@ function getProduct() {
       image: "/images/alphabetGame.jpg",
       title: "Alphabet Game",
       type:"games",
-      discribtion:
-        "It helps the child to focus and connect the shapes to the correct letters. The child can also play with the pieces as cubes and assemble them into different shapes.",
-      price: "50$",
+      price: "50$"
     },
 
     {
       image: "/images/numbersAndShapes.jpg",
       title: "Number-Shape Game",
       type:"games",
-      discribtion:
-        "A 4-in-1 number learning toy for children that helps children learn counting, numbers, shapes, and colors. It contains many activities that help children develop their skills and enjoy their time. ﻿The game includes four different sets of cards with illustrations to help children recognize numbers, shapes and colours.",
-      price: "50$",
+      price: "50$"
     },
 
     {
-      image: "/images/Story1.jpg",
+      image: "/images/wordGame.jpg",
       title: "Words Game",
       type:"games",
-      discribtion:
-        "A 4-in-1 number learning toy for children that helps children learn counting, numbers, shapes, and colors. It contains many activities that help children develop their skills and enjoy their time. ﻿The game includes four different sets of cards with illustrations to help children recognize numbers, shapes and colours.",
-      price: "50$",
-    }
+      price: "50$"
+    },//sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+    {
+        image: "/images/Story1.jpg",
+        title: "The Guardians of bore",
+        type:"story",
+        price: "50$"
+      },
+      {
+        image: "/images/hippos play.jpg",
+        title: "Hippos Play",
+        type:"story",
+        price: "50$"
+      },
+      {
+        image: "/images/the cricket chirps.jpg",
+        title: "Why the cricket chirps",
+        type:"story",
+        discribtion:
+          "A 4-in-1 number learning toy for children that helps children learn counting, numbers, shapes, and colors. It contains many activities that help children develop their skills and enjoy their time. ﻿The game includes four different sets of cards with illustrations to help children recognize numbers, shapes and colours.",
+        price: "50$",
+      },
+      {
+        image: "/images/Gemma.jpg",
+        title: "Gemma",
+        type:"story",
+        discribtion:
+          "A 4-in-1 number learning toy for children that helps children learn counting, numbers, shapes, and colors. It contains many activities that help children develop their skills and enjoy their time. ﻿The game includes four different sets of cards with illustrations to help children recognize numbers, shapes and colours.",
+        price: "50$",
+      },
+      {
+        image: "/images/book1.jpg",
+        title:"Math",
+        type:"book",
+        price: "50$"
+      },
+      {
+        image:"/images/book2.jpg",
+        title:"Back to school",
+        type:"book",
+        discribtion:
+          "A 4-in-1 number learning toy for children that helps children learn counting, numbers, shapes, and colors. It contains many activities that help children develop their skills and enjoy their time. ﻿The game includes four different sets of cards with illustrations to help children recognize numbers, shapes and colours.",
+        price: "50$",
+      },
+      {
+        image:"/images/book3.jpg",
+        title:"Learn to write",
+        type:"book",
+        price: "50$",
+      }
   ];
   return product;
 }
@@ -40,14 +82,12 @@ function draw(data) {
   var div = document.createElement("div");
   var img = document.createElement("img");
   var title = document.createElement("h3");
-  var des = document.createElement("p");
   var price = document.createElement("h4");
   var but = document.createElement("button");
 
   div.classList = "card";
   img.src = data.image;
   title.innerText = data.title;
-  des.innerText = data.discribtion;
   price.innerText = data.price;
   div.style.border = "1px solid black";
   but.innerText = "Add to cart";
@@ -57,7 +97,7 @@ function draw(data) {
     localStorage.setItem("Cards", JSON.stringify(arrOfCards));
     console.log(arrOfCards);
   };
-  div.append(img, title, des, price, but);
+  div.append(img, title, price, but);
   var Div = document.getElementById("cards");
   Div.appendChild(div);
 }
@@ -65,7 +105,7 @@ let arrFromStorage = JSON.parse(localStorage.getItem("Cards"));
 if(arrFromStorage){
     arrOfCards = arrFromStorage;
 }
-console.log(arrFromStorage);
+
 
 
 function search() {
@@ -83,37 +123,66 @@ function search() {
     var p = document.createElement("h4");
     p.innerText = "NO Result";
     div.append(p);
-    var Div = document.getElementById("car  ds");
-    Div.appendChild(div);
+    var Div = document.getElementById("cards");
+    Div.appendChild(div);   
   }
 }
 
-var check=[];
-check=document.getElementsByClassName("fi");
-for(var i=0;check.length;i++){
-console.log(check.value);
+function game(){
+    var idcheck = document.getElementById("games");
+    if(idcheck.checked==true){
+        var div = document.getElementById("cards");
+        div.innerHTML="";
+    for(var i=0;i<product.length;i++){
+        if(product[i].type=="games")
+            draw(product[i])
+        }
+    }
+        else{
+        var div = document.getElementById("cards");
+        div.innerHTML="";
+        for(var i=0;i<product.length;i++)
+            draw(product[i])
+        }
+    }
+function story(){
+    var idcheck = document.getElementById("Stories");
+    if(idcheck.checked==true){
+    var div = document.getElementById("cards");
+    div.innerHTML="";
+    for(var i =0 ; i<product.length;i++){
+        if(product[i].type=="story")
+            draw(product[i]);
+    }
+    }
+    else{
+        var div = document.getElementById("cards");
+        div.innerHTML="";
+        for(var i=0;i<product.length;i++){
+            draw(product[i]);
+        }
+    }
+
+
 }
-    for(var i = 0 ;i<check.length;i++){
-        if(check[i].checked==true){
-            console.log("sssss")
-            var div =  document.getElementById("cards");
-            div.innerHTML="";
-            for(var j=0;j<product.length;j++){
-            if(check.value==product[j].type){
-                    console.log("success");
-            }
+function book(){
+    var idcheck = document.getElementById("Books");
+    if(idcheck.checked==true){
+    var div = document.getElementById("cards");
+    div.innerHTML="";
+    for(var i =0 ; i<product.length;i++){
+        if(product[i].type=="book")
+            draw(product[i]);
+    }
+    }
+    else{
+        var div = document.getElementById("cards");
+        div.innerHTML="";
+        for(var i=0;i<product.length;i++){
+            draw(product[i]);
         }
     }
 }
 
 
-// var gamesBut = document.getElementById("games")
-// var booksBut = document.getElementById("Books")
-// var storiesBut = document.getElementById("Stories")
-// var div =  document.getElementById("cards");
 
-
-//     div.innerHTML="";
-//     for(var i=0;i<product.length;i++){
-//         if(gamesBut.value==product.type)
-//     }
